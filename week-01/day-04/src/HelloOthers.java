ec92f237a0727abe88c6394ead51c35e4cfbfcd2
Matyas Molnar <molnarmatyas92@gmail.com>
2018-05-10 13:15:59 +0200

    printexcercises done

--- week-01/day-04/src/HelloOthers.java
+++ week-01/day-04/src/HelloOthers.java
@@ -2,11 +2,6 @@ public class HelloOthers {
     public static void main(String[] args) {
         System.out.println("Hello Akos!");
         System.out.println("Hello Zsuzsa");
-        // Greet 3 of your classmates with this program, in three separate lines
-        // like:
-        //
-        // Hello, Esther!
-        // Hello, Mary!
-        // Hello, Joe!
+        System.out.println("Hello, Everyone!");
     }
 }
