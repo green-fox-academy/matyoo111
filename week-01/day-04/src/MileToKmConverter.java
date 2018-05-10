2b871b12fe548b665d7645c11a7d537465b9e910
Matyas Molnar <molnarmatyas92@gmail.com>
2018-05-10 17:29:43 +0200

    kmtomiles

---
+++ week-01/day-04/src/MileToKmConverter.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class MileToKmConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int userinputkm = scanner.nextInt();
+        double miles = userinputkm / 1.6;
+        System.out.println(userinputkm + " is approximately" + miles + " miles");
+    }
+}
