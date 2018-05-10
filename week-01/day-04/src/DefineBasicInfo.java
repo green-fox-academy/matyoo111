a81f46a70f426bcd2edea2d98fce8e5ab7890691
Matyas Molnar <molnarmatyas92@gmail.com>
2018-05-10 14:18:26 +0200

    definebasicinfo

---
+++ week-01/day-04/src/DefineBasicInfo.java
@@ -0,0 +1,8 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        String myName = "Matyas";
+        int myAge = 25;
+        double myHeightInMeters = 1.93;
+        boolean iAmMarried = false;
+    }
+}
