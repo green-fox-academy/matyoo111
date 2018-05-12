83d4e357bf66bd0d137b6455f03bf8a3ed66fe15
Matyas Molnar <molnarmatyas92@gmail.com>
2018-05-12 11:48:36 +0200

    diamondrdy2

--- week-01/day-04/src/Diamond.java
+++ week-01/day-04/src/Diamond.java
@@ -1,13 +1,13 @@
 public class Diamond {
     public static void main(String[] args) {
-        int diamondnum = 8;
+        int diamondnum = 5;
         int b = diamondnum;
         int a = diamondnum;
         for (a = 0; a < diamondnum; a++) {
             for (b = 0; b < diamondnum - a; b++) {
                 System.out.print(" ");
             }
-            for (b = diamondnum - a + 1; b < diamondnum + a; b++) {
+            for (b = diamondnum - a; b < diamondnum + a; b++) {
                 System.out.print("*");
             }
             System.out.println("*");
@@ -16,7 +16,7 @@ public class Diamond {
             for (b = 0; b < a - diamondnum; b++) {
                 System.out.print(" ");
             }
-            for (b = a - diamondnum + 1; b < diamondnum * 3 + 1 -a ; b++) {
+            for (b = a - diamondnum -1; b < diamondnum * 3 -a ; b++) {
                 System.out.print("*");
             }
             System.out.println("");
