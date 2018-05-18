662e49d0a3cc25c0502c9adbb4859d5b49da3074
Matyas Molnar <molnarmatyas92@gmail.com>
2018-05-18 14:07:42 +0200

    lineplay for with more squares

--- week-02/day-05/src/LinePlayQuarters.java
+++ week-02/day-05/src/LinePlayQuarters.java
@@ -5,32 +5,54 @@ import java.awt.*;
 import static java.awt.Component.LEFT_ALIGNMENT;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class LinePlay {
+public class LinePlayQuarters {
   public static void mainDraw(Graphics graphics) {
+    int divide = 1;
     int xInit = 0;
     int yInit = 0;
-    int xEnd = WIDTH;
-    int yEnd = 0;
+    int xEnd = WIDTH / divide;
+    int yEnd = HEIGHT / divide;
+    for (int i = 0; i < divide; i++) {
+      for (int j = 0; j < divide; j++) {
+        drawLinePlay(divide, xInit, yInit, xEnd, yEnd, graphics);
+        yInit = 0;
+        xInit = xInit + (WIDTH / divide) * j;
+        yEnd = yEnd + (HEIGHT / divide) * j;
+        xEnd = 0;
+        }
+        yEnd = yInit + (WIDTH / divide) * i;
+        xInit = xInit + (WIDTH / divide) * i;
+        yInit = yInit + HEIGHT / divide * i;
+        xEnd = xEnd + (WIDTH / divide) * i;
+    }
+  }
+  public static void drawLinePlay(int divide, int xInit, int yInit, int xEnd, int yEnd,Graphics graphics) {
+    int xEnd1 = xEnd;
+    int yEnd1 = yEnd;
+    int xInit1 = xInit;
+    int yInit1 = yInit;
     Color color = new Color(219, 102, 201);
     for (int i = 0; i <= WIDTH / 20; i++) {
-      drawLine(xInit, yInit, xEnd, yEnd, color, graphics);
-      xInit = xInit + 20;
-      yEnd= yEnd + 20;
+      drawLine(xInit1, yInit1, xEnd1, yEnd1, color, graphics);
+      xInit1 = xInit1 + 20 / divide;
+      yEnd1 = yEnd1 + 20 / divide;
     }
     color = Color.GREEN;
-    xInit = 0;
-    yInit = 0;
-    xEnd = 0;
-    yEnd = HEIGHT;
+    int xEnd2 = xEnd;
+    int yEnd2 = yEnd;
+    int xInit2 = xInit;
+    int yInit2 = yInit;
     for (int i = 0; i <= HEIGHT / 20; i++) {
-      drawLine(xInit, yInit, xEnd, yEnd, color, graphics);
-      yInit = yInit + 20;
-      xEnd = xEnd + 20;
+      drawLine(xInit2, yInit2, xEnd2, yEnd2, color, graphics);
+      yInit2 = yInit2 + 20 / divide;
+      xEnd2 = xEnd2 + 20 / divide;
     }
-
-
-
   }
+
+
+
+
+
   public static void drawLine(int xInit, int yInit, int xEnd, int yEnd, Color color, Graphics graphics) {
     graphics.setColor(color);
     graphics.drawLine(xInit, yInit, xEnd, yEnd);
