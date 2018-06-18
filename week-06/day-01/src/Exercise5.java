3e8a068572c86c78c7d73076750ba1dd07346c55
Matyas Molnar <molnarmatyas92@gmail.com>
2018-06-18 12:49:46 +0200

    excercise 5 done

--- week-06/day-01/src/Exercise5.java
+++ week-06/day-01/src/Exercise5.java
@@ -1,7 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Exercise5 {
 
   public static void main(String[] args) {
 
     String exampleString = "Find the UpperCase Characters IN A STRING";
+    
+    exampleString.chars()
+            .mapToObj(n -> (char) n)
+            .filter(n -> n.toString().equals(n.toString().toUpperCase()))
+            .forEach(System.out::println);
+
+
   }
 }
