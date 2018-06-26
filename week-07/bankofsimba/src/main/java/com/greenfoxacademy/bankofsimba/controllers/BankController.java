79dd4188d37dd65c4e8014e766be484752d8ee66
Matyas Molnar <molnarmatyas92@gmail.com>
2018-06-26 13:16:37 +0200

    bankofsimba tables shown

--- week-07/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/controllers/BankController.java
+++ week-07/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/controllers/BankController.java
@@ -4,7 +4,10 @@ import com.greenfoxacademy.bankofsimba.models.BankAccount;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -12,10 +15,29 @@ public class BankController {
 
   List<BankAccount> accounts;
 
+  public BankController() {
+    //accounts = new ArrayList<>();
+  }
+
+
+
   @GetMapping("")
   public String showAccount(Model model) {
-    BankAccount account1 = new BankAccount("Simba", 2000, "Lion");
-    model.addAttribute("bankaccount", account1);
+    accounts = Arrays.asList(
+            new BankAccount("Simba", 2000, "Lion"),
+            new BankAccount("Nala", 300, "Lion"),
+            new BankAccount("Rafiki", 100, "Monkey"),
+            new BankAccount("Gopher", 800, "Gopher"),
+            new BankAccount("Speedy", 500, "Snail")
+    );
+
+    model.addAttribute("bankaccounts", accounts);
     return "index";
+  }
+
+  @GetMapping("/registration")
+  public String trial(Model model){
+    model.addAttribute("text", "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>");
+    return "registration";
   }
 }
